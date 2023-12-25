d2d1672e39f8c695616d4fe4cb6a70d109bf05bf
dortal <dortal721@gmail.com>
2023-12-25 21:35:14 +0200

    Integrated Post saving functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index_page'), 
     path('all-posts', views.AllPostsView.as_view(), name='all_posts'),
-    path('all_posts/<slug:slug>', views.SinglePostView.as_view(), name='single_post')
+    path('all_posts/save-post', views.ReadLaterView.as_view(), name='save_post'),
+    path('all_posts/saved-posts', views.SavedPostsView.as_view(), name='saved_posts'),
+    path('all_posts/<slug:slug>', views.SinglePostView.as_view(), name='single_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -25,26 +25,36 @@ class AllPostsView(ListView):
     ordering = ["-created_date"] 
     context_object_name = "posts" 
 
-class SinglePostView(View): 
+def is_post_saved(request, post_obj: Post): 
+    stored_posts = request.session.get('stored_posts') 
+
+    if stored_posts is None: 
+        return False 
+    
+    return post_obj.id in stored_posts
+
+class SinglePostView(View):
+
     def get(self, request, slug): 
-        cur_post = Post.objects.get(slug=slug)
+        cur_post = Post.objects.get(slug=slug) 
     
         return render(request, 'blog/post-detail.html', 
                       {'blog_data': cur_post, 
                        'comment_form': CommentForm(), 
-                       'comments': cur_post.comments.all()})
+                       'comments': cur_post.comments.all().order_by("-id"), 
+                       'is_saved': is_post_saved(request, cur_post)})
     
-    def post(self, request, slug): 
+    def post(self, request, slug):
         comment_form = CommentForm(request.POST) 
+        cur_post = Post.objects.get(slug=slug)
 
         if comment_form.is_valid(): 
             cur_comment = comment_form.save(commit=False)
             
             # assigning post id by slug 
-            cur_post = Post.objects.get(slug=slug) 
             cur_comment.post_id = cur_post 
 
-            cur_comment.save() 
+            cur_comment.save()
 
             # redirecting a GET request to the original page 
             return HttpResponseRedirect(reverse("single_post", args=[slug]))
@@ -52,4 +62,51 @@ class SinglePostView(View):
         return render(request, 'blog/post-detail.html', 
                           {'blog_data': cur_post, 
                            'comment_form': comment_form, 
-                           'comments': cur_post.cur_post.comments.all()})
+                           'comments': cur_post.cur_post.comments.all().order_by("-id")}) 
+    
+class ReadLaterView(View):
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        # updating post to saved list 
+        cur_post_id = int(request.POST['post_id'])
+
+        if stored_posts is None: 
+            stored_posts = [cur_post_id] 
+        elif cur_post_id not in stored_posts: 
+            stored_posts.append(cur_post_id) 
+        else: 
+            stored_posts.remove(cur_post_id)
+            
+        request.session['stored_posts'] = stored_posts
+        cur_post_obj = Post.objects.get(id=cur_post_id)
+
+        # redirecting a GET request to post page
+        return HttpResponseRedirect(reverse('single_post', args=[cur_post_obj.slug])) 
+    
+
+class SavedPostsView(View):
+    def get(self, request): 
+        # obtaining posts stored by the user 
+        stored_posts_ids = request.session.get("stored_posts")
+
+        # if None - output empty list 
+        if stored_posts_ids is None: 
+            return render(request, 'blog/saved-posts.html', 
+                          {'posts': None}) 
+        
+        # if not None, but still empty - output empty list 
+        if not len(stored_posts_ids): 
+            return render(request, 'blog/saved-posts.html', 
+                          {'posts': None}) 
+        
+        # otherwise - output full list 
+        stored_posts = Post.objects.filter(id__in=stored_posts_ids)
+
+        return render(request, 'blog/saved-posts.html', 
+                          {'posts': stored_posts})
+
+            
+
+            
+
